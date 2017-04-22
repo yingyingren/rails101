e8950c34c0e6c09ac328b1a4f911bf23afedf380
ryy <meizhuangxihu@gmail.com>
2017-04-22 11:18:47 +0800

    add post and hook group/user relation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -39,7 +39,7 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-  @group.destroy
+     @group.destroy
     redirect_to groups_path, alert: "Group deleted"
   end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :group
-
-  validates :content, presence: true
-
 end
